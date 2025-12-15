37f5e5a7ccc2db43e96186e12f42761a44d2ed31
Gbesh <gbesh.12@gmail.com>
2025-12-15 22:56:44 +0000

    Added base and hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    {/* <HashRouter> */}
       <App />
-    </HashRouter>
+    {/* </HashRouter> */}
   </StrictMode>
 );
